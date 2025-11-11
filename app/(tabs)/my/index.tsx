c8141d9b00ec173a5d58ff37f14abd698efd35fb
MS-Kwak <kkminseo@gmail.com>
2025-11-11 12:11:36 +0900

    Initial commit

---
+++ app/(tabs)/my/index.tsx
@@ -0,0 +1,14 @@
+import { router, useFocusEffect } from "expo-router";
+import { SafeAreaView, Text } from "react-native";
+
+export default function MyScreen() {
+  useFocusEffect(() => {
+    router.replace("/auth");
+  });
+
+  return (
+    <SafeAreaView>
+      <Text>내정보 스크린</Text>
+    </SafeAreaView>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,45 @@
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "@/api/queryClient";
+import useAuth from "@/hooks/queries/useAuth";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootNavigator />
+    </QueryClientProvider>
+  );
+}
+
+function RootNavigator() {
+  const { auth } = useAuth();
+  console.log("auth", auth);
+
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="auth" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
