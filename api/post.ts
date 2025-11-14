e94fff35447d398f116479a79a71f992b3d73086
MS-Kwak <kkminseo@gmail.com>
2025-11-15 07:09:22 +0900

    스와이프(Swipe) 탭 구현하기

--- api/post.ts
+++ api/post.ts
@@ -18,6 +18,18 @@ async function getPosts(page = 1): Promise<Post[]> {
   return data;
 }
 
+async function getMyPosts(page = 1): Promise<Post[]> {
+  const { data } = await axiosInstance.get(`/posts/my?page=${page}`);
+
+  return data;
+}
+
+async function getLikedPosts(page = 1): Promise<Post[]> {
+  const { data } = await axiosInstance.get(`/likes?page=${page}`);
+
+  return data;
+}
+
 async function deletePost(id: number): Promise<number> {
   const { data } = await axiosInstance.delete(`/posts/${id}`);
 
@@ -72,4 +84,6 @@ export {
   updatePost,
   createVote,
   likePost,
+  getMyPosts,
+  getLikedPosts,
 };
